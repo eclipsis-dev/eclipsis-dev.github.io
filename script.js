f4dff5f667840b2ad0369bb7588dc1f4284f3afa
lunareclipse <obscure808080@gmail.com>
2017-02-28 17:39:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/*$(document).ready(function() {
+    $('.blocks-right-items').mouseenter(function() {
+        $('.blocks-right-items').fadeTo('fast', 0.5);
+    });
+    $('.blocks-right-items').mouseleave(function() {
+        $('.blocks-right-items').fadeTo('fast', 1);
+    });
+});*/
